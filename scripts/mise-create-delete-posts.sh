33763b8c634d072e57567a87ec39ef13ef1961b9
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-06-25 16:58:35 +0200

    chore: update cloudflare pages deployment action to use wrangler-action (#391)

--- scripts/mise-create-delete-posts.sh
+++ scripts/mise-create-delete-posts.sh
@@ -50,8 +50,8 @@ if grep -Eq '(^| )eksctl ' "${RUN_FILE}"; then
     echo "export AWS_DEFAULT_REGION=\"${AWS_DEFAULT_REGION}\""
     # shellcheck disable=SC2028
     echo "eval \"\$(aws sts assume-role --role-arn \"\${AWS_ROLE_TO_ASSUME}\" --role-session-name \"\$USER@\$(hostname -f)-\$(date +%s)\" --duration-seconds 36000 | jq -r '.Credentials | \"export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\\nexport AWS_SESSION_TOKEN=\(.SessionToken)\\n\"')\""
-    echo "export KUBECONFIG=\"/tmp/kubeconfig-${CLUSTER_NAME}.conf\""
-    echo "aws eks update-kubeconfig --region \"${AWS_DEFAULT_REGION}\" --name \"${CLUSTER_NAME}\" --kubeconfig \"\$KUBECONFIG\""
+    echo "export KUBECONFIG=\"/tmp/kubeconfig-${CLUSTER_NAME}-\$\$.conf\""
+    echo "aws eks update-kubeconfig --region \"${AWS_DEFAULT_REGION}\" --name \"${CLUSTER_NAME}\" --kubeconfig \"\${KUBECONFIG}\""
     echo '```'
   ) | tee "${GITHUB_STEP_SUMMARY}"
 fi
