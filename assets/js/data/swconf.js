3ce2e8a442c194ce7ee9032eba5e192a59a33af6
ruzickap <ruzickap@users.noreply.github.com>
2025-11-27 07:45:10 +0000

    deploy: a50d90edcd2102167414e3bf5fcd897884c24cb5

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1764229502',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/projects/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ 'https://ruzickap-github-io.goatcounter.com/counter/' ] }, purge: false };
