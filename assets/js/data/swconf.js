f6a2b2dce2857ab03b40b7d272d8066315c0c084
ruzickap <ruzickap@users.noreply.github.com>
2026-01-01 07:54:01 +0000

    deploy: 0778a2d845197170faff66e51312892972233b63

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1767254034',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/projects/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ 'https://ruzickap-github-io.goatcounter.com/counter/' ] }, purge: false };
